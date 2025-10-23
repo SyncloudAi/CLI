4abb9e93e36ab8dadb8808dde5b0090f656f4352
Gggggggggbbb <gabiregev@gmail.com>
2025-10-23 19:21:18 +0300

    Update version from dev0.4.7 to dev0.4.8

--- macdev.sh
+++ macdev.sh
@@ -2,7 +2,7 @@
 set -euo pipefail # Ensures script exits immediately on errors (pipefail)
 IFS=$'\n\t'
 
-VERSION="dev0.4.7"
+VERSION="dev0.4.8"
 REPO="SyncloudAi/CLI"
 INSTALL_DIR="$HOME/.local/bin"
 
